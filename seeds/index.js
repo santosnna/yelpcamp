31cb6d1955ff1f2ddeb0e94d26c24d4e39a5f62f
Nathan Santos <santos.nna@outlook.com>
2023-05-02 01:38:19 +0200

    Upload commit: This project already contains a CRUD with database connection, basic styling, error and data validation.

---
+++ seeds/index.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+const Campground = require("../models/campground");
+
+const cities = require("./cities");
+const { places, descriptors } = require("./seedHelpers");
+
+mongoose.connect("mongodb://localhost:27017/yelp-camp", {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+	console.log("Database connected");
+});
+
+const sample = (array) => array[Math.floor(Math.random() * array.length)];
+
+const seedDB = async () => {
+	await Campground.deleteMany({});
+	for (let i = 0; i < 50; i++) {
+		const random1000 = Math.floor(Math.random() * 1000);
+		const price = Math.floor(Math.random() * 20) + 10;
+		const camp = new Campground({
+			title: `${sample(descriptors)} ${sample(places)}`,
+			location: `${cities[random1000].city}, ${cities[random1000].state}`,
+			// This line needs to change. Check solution in Q&A
+			image: `https://source.unsplash.com/random/300x300?camping,${i}`,
+			description:
+				"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus animi pariatur, tempora aperiam provident, in nemo nulla consequatur eaque harum quaerat officiis sint facilis quas laboriosam, aliquam beatae eum quasi?",
+			price,
+		});
+		await camp.save();
+	}
+};
+
+seedDB().then(() => {
+	mongoose.connection.close();
+});
