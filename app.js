7bf3110d2a3396e112f9d84a750913312c358361
Nathan Santos <santos.nna@outlook.com>
2023-05-02 02:05:44 +0200

    Created a New Review route

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const catchAsync = require("./utils/catchAsync");
 const ExpressError = require("./utils/ExpressError");
 const methodOverride = require("method-override");
 const Campground = require("./models/campground");
+const Review = require("./models/review");
 
 const app = express();
 
@@ -98,6 +99,18 @@ app.delete(
 		const { id } = req.params;
 		await Campground.findByIdAndDelete(id);
 		res.redirect("/campgrounds");
+	})
+);
+
+app.post(
+	"/campgrounds/:id/reviews",
+	catchAsync(async (req, res) => {
+		const campground = await Campground.findById(req.params.id);
+		const review = new Review(req.body.review);
+		campground.reviews.push(review);
+		await review.save();
+		await campground.save();
+		res.redirect(`/campgrounds/${campground._id}`);
 	})
 );
 
