17dc10f0d1c547d96f1ba05c03996a6d095bffe3
Nathan Santos <santos.nna@outlook.com>
2023-05-09 14:23:17 +0200

    isLoggedIn Middleware

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -4,6 +4,7 @@ const catchAsync = require("../utils/catchAsync");
 const ExpressError = require("../utils/ExpressError");
 const Campground = require("../models/campground");
 const { campgroundSchema } = require("../schemas");
+const { isLoggedIn } = require("../middleware");
 
 /**
  * This function is a Middleware that will validate a
@@ -28,12 +29,13 @@ router.get("/", async (req, res) => {
 	res.render("campgrounds/index", { campgrounds });
 });
 
-router.get("/new", (req, res) => {
+router.get("/new", isLoggedIn, (req, res) => {
 	res.render("campgrounds/new");
 });
 
 router.post(
 	"/",
+	isLoggedIn,
 	validateCampground,
 	catchAsync(async (req, res, next) => {
 		// if (!req.body.campground)
@@ -62,6 +64,7 @@ router.get(
 
 router.get(
 	"/:id/edit",
+	isLoggedIn,
 	catchAsync(async (req, res) => {
 		const campground = await Campground.findById(req.params.id);
 		if (!campground) {
@@ -74,6 +77,7 @@ router.get(
 
 router.put(
 	"/:id",
+	isLoggedIn,
 	validateCampground,
 	catchAsync(async (req, res) => {
 		const { id } = req.params;
@@ -87,6 +91,7 @@ router.put(
 
 router.delete(
 	"/:id",
+	isLoggedIn,
 	catchAsync(async (req, res) => {
 		const { id } = req.params;
 		await Campground.findByIdAndDelete(id);
