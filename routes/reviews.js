86bd655cd2a20e816e6ba0d510fe5c959c98c8b5
Nathan Santos <santos.nna@outlook.com>
2023-06-26 15:01:15 +0200

    Refactored to Reviews Controller

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true }); // This option is necessary because routers get separate params and the params would not reach this route otherwise
 const catchAsync = require("../utils/catchAsync");
-const Campground = require("../models/campground");
-const Review = require("../models/review");
+const reviews = require("../controllers/reviews");
 const { validateReview, isLoggedIn, isReviewAuthor } = require("../middleware");
 
 // REVIEW ROUTES
@@ -10,29 +9,14 @@ router.post(
 	"/",
 	isLoggedIn,
 	validateReview,
-	catchAsync(async (req, res) => {
-		const campground = await Campground.findById(req.params.id);
-		const review = new Review(req.body.review);
-		review.author = req.user._id;
-		campground.reviews.push(review);
-		await review.save();
-		await campground.save();
-		req.flash("success", "Created new review!");
-		res.redirect(`/campgrounds/${campground._id}`);
-	})
+	catchAsync(reviews.createNewReview)
 );
 
 router.delete(
 	"/:reviewId",
 	isLoggedIn,
 	isReviewAuthor,
-	catchAsync(async (req, res) => {
-		const { id, reviewId } = req.params;
-		await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } }); // This method is used to remove elements from arrays in MongoDB
-		await Review.findByIdAndDelete(reviewId);
-		req.flash("success", "Successfully deleted review!");
-		res.redirect(`/campgrounds/${id}`);
-	})
+	catchAsync(reviews.deleteReview)
 );
 
 module.exports = router;
